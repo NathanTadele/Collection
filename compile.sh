4089fba278eedcbc2ebecb1e2103feb26afaaa53
Nahumtariku <Tarikunahom@yahoo.com>
2018-04-30 23:36:53 -0600

    Add files via upload

--- compile.sh
+++ compile.sh
@@ -4,20 +4,20 @@ compile() {
     clear
     echo "Preparing Compile"
     rm game
-    echo ""          #removes current game.exec
+    echo ""
     echo "Compiling game"
     echo ""
-    make                #runs makefile to compile individual .c files
-    #add .o files below
+    make
+
     gcc album.o array.o collection.o -o collection
-    if [ -e collection ]      #checks if game.exec was made
+    if [ -e collection ]
         then {
             echo ""
             echo "Compile Successful"
             echo ""
         } else {
             echo "Compile Failure"
-            exit -1     #if file was not made compilation fails
+            exit -1    
         }
     fi
     # clear
